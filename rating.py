40b2ebcb6e7639fb382d557b70c1759bf8390dce
rial99 <ratulstephenite99@gmail.com>
2018-04-27 16:03:43 +0530

    v2

--- rating.py
+++ rating.py
@@ -1,18 +1,23 @@
+import time
 import urllib.request
 import sys 
 from pprint import pprint
 import json
+
 url = "https://koinex.in/api/ticker"
 
-def data_parser(coin):
+def data_parser(coin,what):
     req = urllib.request.Request(url)
     r=urllib.request.urlopen(req).read()
     cont = json.loads(r.decode('utf-8'))
-    pprint(cont['prices'][coin])
+    pprint(cont['stats'][coin][what])
 
 def main():
 	par_1 = sys.argv[1]
-	data_parser(par_1)
+	par_2 = sys.argv[2]
+	while (True):
+		time.sleep(5)
+		data_parser(par_1,par_2)
 
 if __name__ == '__main__':
 	main()
